157363f082e4cfaf0a0fad9f2aaa97f030e61173
Carlos <clockwerkz@gmail.com>
2020-04-28 13:06:34 -0700

    adds concurrently, removes static public folder

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.DB_URI, {
     .catch((err) => console.error("Error:",err));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'));
+
 
 
 app.get('/test', (req,res)=> {
