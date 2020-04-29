61f8545b3b0db6c18ed0297a46d2dbcbc722fe83
Carlos <clockwerkz@gmail.com>
2020-04-29 11:24:33 -0700

    upating signup sheet

--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -1,13 +1,40 @@
 <template>
-    <h2>SignUp Page</h2>
+    <div>
+        <h2>Signup Page</h2>
+        <p>{{error}}</p>
+        <input type="email" id="email" v-model="email" placeholder="Enter your email"/>
+        <input type="password" id="password" v-model="password" placeholder="Enter your password" />
+        <input type="password" id="password2" v-model="password2" placeholder="Re-enter your password" />
+        <button v-on:click="signup()">Login</button>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'login',
-    username: '',
-    password: '',
+    name: 'signup',
+    data() {
+        return {
+            email: ' ',
+            password: '',
+            password2: '',
+            error: '' 
+        }
+    },
     methods: {
+        signup() {
+            if (!this.email) {
+                return this.error = "Please enter a password";
+            }
+            if (this.password==="") {
+                return this.error ="Password field cannot be empty";
+            }
+            if (this.password !== this.password2) {
+                return this.error = "Passwords do not match";
+            }
+        },
+        clearError() {
+            this.error = "";
+        }
     }
 }
 </script>
